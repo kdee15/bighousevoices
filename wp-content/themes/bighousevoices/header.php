6feadd543047a88f0bd9ed9f2c077d394f882976
Midas Mtileni <midas@forgetech.net>
2016-02-25 23:53:30 +0200

    Theme setup, running

---
+++ wp-content/themes/bighousevoices/header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Tutorial theme</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/stylesheet.css">
+
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/components.js"></script>
+
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/app.js"></script>
+
+
+</head>
+<body>
+<div id="headerbar">Header</div>
+<div id="wrapper">
+<div id="header">
+<h1>HEADER</h1>
+</div>
