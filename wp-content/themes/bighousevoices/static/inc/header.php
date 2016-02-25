d98c6e6e18cafe1721cf3c09e42d64386ecb6617
Kirk Daniels <kdee15@gmail.com>
2016-02-25 15:29:38 +0200

    static build

---
+++ wp-content/themes/bighousevoices/static/inc/header.php
@@ -0,0 +1,88 @@
+    <header class="masthead grid">
+
+            <!-- C.1.1. MASTHEAD LOGO -->
+
+            <figure class="masthead__logo one-quarter">
+                <img src="../assets/images/site/logo.png" alt="Matt Silver-Vallance" />
+            </figure>
+
+            <!-- C.1.1. END -->
+
+            <!-- C.1.2. NAVIGATION -->
+
+            <div class="masthead__nav one-half">
+
+                <!-- C.1.2.1. Burger Button -->
+
+                <a href="#burger-menu" class="reveal burger-button">
+
+                    <div class="line line-1"></div>
+                    <div class="line line-2"></div>
+                    <div class="line line-3"></div>
+
+                </a>
+
+                <!-- C.1.2.1. End -->
+
+                <!-- C.1.2.2. Main Navigation -->
+
+                <nav class="primary nav">
+
+                    <ul>
+                        <li><a href="#" class="active"><span>Home</span></a></li>
+                        <li><a href="#"><span>Bio</span></a></li>
+                        <li><a href="#"><span>Projects</span></a></li>
+                        <li><a href="#"><span>Press</span></a></li>
+                        <li><a href="#"><span>Blog</span></a></li>
+                        <li><a href="#"><span>Speaker</span></a></li>
+                        <li><a href="#"><span>Contact</span></a></li>
+                    </ul>
+
+                </nav>
+
+                <!-- C.1.2.2. End -->
+
+                <!-- C.1.2.3. Social Navigation -->
+
+                <nav class="nav-social nav">
+
+                    <ul>
+                        <li>
+                            <a href="http://www.linkedin.com/profile/view?id=89924610" target="_blank">
+                                <img src="../assets/images/site/icon-linkedIn.svg" alt="Connect with Matt Silver-Vallance on LinkedIn" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="../assets/images/site/icon-facebook.svg" alt="Add Matt Silver-Vallance on Facebook" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/balloonbloke" target="_blank">
+                                <img src="../assets/images/site/icon-twitter.svg" alt="Follow Matt Silver-Vallance on Twitter" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCq9Rvjt8EGKpLbPmZVLK3Rw/videos" target="_blank">
+                                <img src="../assets/images/site/icon-youtube.svg" alt="Watch what Matt Silver-Vallance get's up to next" />
+                            </a>
+                        </li>
+                    </ul>
+
+                </nav>
+
+                <!-- C.1.2.3. End -->
+
+            </div>
+
+            <!-- C.1.2. END -->
+
+            <!-- C.1.3. NEWSLETTER SIGN-UP -->
+            <nav class="newsletter one-quarter">
+
+                <img src="../assets/images/site/button-newsletter.png" alt="Sign up for Matt's Newsletter" />
+
+            </nav>
+            <!-- C.1.3. END -->
+
+    </header>
