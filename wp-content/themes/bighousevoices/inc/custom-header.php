9c90402074288a5985a223eeb6bc5fc99e32d939
Kirk Daniels <kdee15@gmail.com>
2016-03-04 20:24:19 +0200

    site design

--- wp-content/themes/bighousevoices/inc/custom-header.php
+++ wp-content/themes/bighousevoices/inc/custom-header.php
@@ -12,37 +12,37 @@
 	<?php endif; // End header image check. ?>
 
  *
- * @package crio-msv
+ * @package bighousevoices
  */
 
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses crio_msv_header_style()
- * @uses crio_msv_admin_header_style()
- * @uses crio_msv_admin_header_image()
+ * @uses bighousevoices_header_style()
+ * @uses bighousevoices_admin_header_style()
+ * @uses bighousevoices_admin_header_image()
  */
-function crio_msv_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'crio_msv_custom_header_args', array(
+function bighousevoices_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'bighousevoices_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => 'crio_msv_header_style',
-		'admin-head-callback'    => 'crio_msv_admin_header_style',
-		'admin-preview-callback' => 'crio_msv_admin_header_image',
+		'wp-head-callback'       => 'bighousevoices_header_style',
+		'admin-head-callback'    => 'bighousevoices_admin_header_style',
+		'admin-preview-callback' => 'bighousevoices_admin_header_image',
 	) ) );
 }
-add_action( 'after_setup_theme', 'crio_msv_custom_header_setup' );
+add_action( 'after_setup_theme', 'bighousevoices_custom_header_setup' );
 
-if ( ! function_exists( 'crio_msv_header_style' ) ) :
+if ( ! function_exists( 'bighousevoices_header_style' ) ) :
 /**
  * Styles the header image and text displayed on the blog
  *
- * @see crio_msv_custom_header_setup().
+ * @see bighousevoices_custom_header_setup().
  */
-function crio_msv_header_style() {
+function bighousevoices_header_style() {
 	$header_text_color = get_header_textcolor();
 
 	// If no custom options for text are set, let's bail
@@ -75,15 +75,15 @@ function crio_msv_header_style() {
 	</style>
 	<?php
 }
-endif; // crio_msv_header_style
+endif; // bighousevoices_header_style
 
-if ( ! function_exists( 'crio_msv_admin_header_style' ) ) :
+if ( ! function_exists( 'bighousevoices_admin_header_style' ) ) :
 /**
  * Styles the header image displayed on the Appearance > Header admin panel.
  *
- * @see crio_msv_custom_header_setup().
+ * @see bighousevoices_custom_header_setup().
  */
-function crio_msv_admin_header_style() {
+function bighousevoices_admin_header_style() {
 ?>
 	<style type="text/css">
 		.appearance_page_custom-header #headimg {
@@ -103,15 +103,15 @@ function crio_msv_admin_header_style() {
 	</style>
 <?php
 }
-endif; // crio_msv_admin_header_style
+endif; // bighousevoices_admin_header_style
 
-if ( ! function_exists( 'crio_msv_admin_header_image' ) ) :
+if ( ! function_exists( 'bighousevoices_admin_header_image' ) ) :
 /**
  * Custom header image markup displayed on the Appearance > Header admin panel.
  *
- * @see crio_msv_custom_header_setup().
+ * @see bighousevoices_custom_header_setup().
  */
-function crio_msv_admin_header_image() {
+function bighousevoices_admin_header_image() {
 	$style = sprintf( ' style="color:#%s;"', get_header_textcolor() );
 ?>
 	<div id="headimg">
@@ -123,4 +123,4 @@ function crio_msv_admin_header_image() {
 	</div>
 <?php
 }
-endif; // crio_msv_admin_header_image
+endif; // bighousevoices_admin_header_image
