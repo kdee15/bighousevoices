d6ef6a23cee3cf77f7dba4087dc367c38b252aac
Kirk Daniels <kdee15@gmail.com>
2016-03-09 10:29:55 +0200

    need to fix

---
+++ wp-content/themes/bighousevoices/assets/js/scripts/konstruct.js
@@ -0,0 +1,23 @@
+
+$(".reveal").on('click touchstart', function (e) {
+
+    var target = $(this).attr('href');
+    
+    if ($(target).css('display') === 'none') {
+      $('.aside-team').addClass('not-active');
+      $(target).addClass('active');
+      $(target).removeClass('not-active');
+    }
+    
+    else {
+      $(target).removeClass('active');
+      $(target).addClass('not-active');
+    }
+    
+    e.preventDefault();
+    
+});
+
+
+
+
